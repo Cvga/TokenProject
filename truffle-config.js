2932c719137d44b60c4f59bf5aeac4d4ea81bb31
CVega <carvega07@gmail.com>
2022-02-21 16:52:15 -0600

    KycFunctionCompleted

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require('dotenv').config({path: './.env'});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
 // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,7 +12,13 @@ networks: {
   port: 7545,
   network_id: 5777,
   host: "127.0.0.1"
-  }
+  },
+  ganache_local: {
+    provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", MetaMaskAccountIndex )
+    },
+    network_id: 5777
+    }
 },
 compilers: {    
     solc: {
